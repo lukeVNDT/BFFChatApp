e8830303e760095a72308e3c677b345d56a8f982
Luke Van <87007106+lukeVNDT@users.noreply.github.com>
2023-02-09 23:49:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,30 @@ Route::get('/messages', [App\Http\Controllers\MessageController::class, 'getmess
 Route::post('/messages', [App\Http\Controllers\MessageController::class, 'sendmessage']);
 Route::post('/activeuser', [App\Http\Controllers\MessageController::class, 'activeuser']);
 
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache has been cleared';
+});
+
+//Clear route cache:
+Route::get('/route-cache', function() {
+	Artisan::call('route:cache');
+    return 'Routes cache has been cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+ 	Artisan::call('config:cache');
+ 	return 'Config cache has been cleared';
+}); 
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'View cache has been cleared';
+});
+
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
